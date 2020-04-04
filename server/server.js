1ec0728ec1879ffedefa7919900d4709e346077e
Zac H <zac.haluza@gmail.com>
2020-04-04 13:34:19 -0400

    added schema

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const PORT = 3000;
 
-app.use('/graphql', graphqlHTTP({}));
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
