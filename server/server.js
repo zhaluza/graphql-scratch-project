c8739941453107c5fddab68564c236f03d148f0a
Zac H <zac.haluza@gmail.com>
2020-04-04 16:26:29 -0400

    connected server to db

--- server/server.js
+++ server/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
-const app = express();
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const schema = require('./schema/schema');
 
+const app = express();
 const PORT = 3000;
+
+// connect to database
+const mongoURI = process.env.MONGO_URI;
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.connection.once('open', () => {
+  console.log('Connected to database');
+});
 
 app.use(
   '/graphql',
