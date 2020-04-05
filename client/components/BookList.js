9c7a86eab0cf4e9415b8966625f4149a81d21e8d
Zac H <zac.haluza@gmail.com>
2020-04-05 12:03:40 -0400

    added select book functionality

--- client/components/BookList.js
+++ client/components/BookList.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { GET_BOOKS } from '../queries/queries';
+import BookDetails from './BookDetails';
 
 const BookList = props => {
+  const [selected, setSelected] = useState(null);
   const { loading, error, data } = useQuery(GET_BOOKS);
   if (loading) return <p>Loading Books...</p>;
   if (error) return <p>Error</p>;
 
   return (
     <div>
-      <ul id="book-list">
+      <ul className="book-list">
         {data.books.map(book => (
-          <li key={book.id}>{book.name}</li>
+          <li key={book.id} onClick={() => setSelected(book.id)}>
+            {book.name}
+          </li>
         ))}
       </ul>
+      <BookDetails bookId={selected} />
+      <button onClick={() => setSelected(null)}>Clear Book</button>
     </div>
   );
 };
