79213c582b5e5abd718355fd46f1022f32bf48c6
Zac H <zac.haluza@gmail.com>
2020-04-04 23:58:34 -0400

    basic apollo integration

---
+++ client/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import BookList from './BookList';
+
+// Apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql'
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div id="main">
+      <h1>My Reading List</h1>
+      <BookList />
+    </div>
+  </ApolloProvider>
+);
+
+export default App;
